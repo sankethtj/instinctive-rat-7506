4d2af52df2e06edd7020f3804bfc15111b95e0be
sankethtj <sankethjaganath@gmail.com>
2022-08-24 18:14:48 +0530

    Added Checkout Page Navbar

---
+++ Components/checkoutNavbar.js
@@ -0,0 +1,23 @@
+function checkoutNavbar(){
+
+    return `
+    <div id="icon-title">
+        <div>
+          <img
+            id="bestbuy-icon"
+            src="https://play-lh.googleusercontent.com/fLTpaW6d3nPupCwmVlfhdHPmw1fnt362QGIz0TATVxVr25XjRiOrfmjGOaOw5jMYu34D"
+            alt="best buy icon"
+          />
+        </div>
+
+        <div class="checkout-header_title">
+          <span>Checkout</span>
+        </div>
+      </div>
+      <a id="returnToCart" href="#">
+        <span>Return to Cart</span>
+      </a>`;
+
+}
+
+export default checkoutNavbar;
